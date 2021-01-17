13f415d8958d9a7b7b4fdaf92b3840f062041d73
Laura <lmcsay@gmail.com>
2021-01-16 22:18:27 -0600

    Feat: Create user route handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+// Automatically parses JSON to an object
+app.use(express.json())
+
+// Route handler
+app.post('/users', (req, res)=> {
+    const user = new User(req.body)
+
+    user.save()
+        .then(() => {res.send(user)})
+        .catch((error) => {
+            res.status(400).send(error)
+        })
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-})
+});
