3f3d6a238f07832b99761e51c214e3246011ccc1
Laura <lmcsay@gmail.com>
2021-01-17 14:47:59 -0600

    Refactor: Update API endpoints to use Async/await

--- src/index.js
+++ src/index.js
@@ -12,67 +12,83 @@ app.use(express.json())
 
 // REST API Endpoints
 // Post new user
-app.post('/users', (req, res)=> {
+app.post('/users', async (req, res)=> {
     const user = new User(req.body)
-
-    user.save()
-        .then(() => res.status(201).send(user))
-        .catch((error) => {
-            res.status(400).send(error.message)
-        })
+    try {
+        await user.save()
+        // res.status will only run if Promise above is fulfilled
+        res.status(201).send(user)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
 });
 
 // Fetch all users
-app.get('/users', (req, res) => {
-    User.find({})
-        .then((users) => res.send(users))
-        .catch((error) => res.status(500).send(error.message))
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch(error) {
+        res.status(500).send(error.message)
+    }
 })
 
 // Fetch user by id
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
     if (!mongoose.isValidObjectId(_id)){
-        return res.status(400).send('Invalid ID!')
+        return res.status(400).send('Invalid ID!') 
     }
-    // mongoose will automatically convert string id into object ID
-    User.findById(_id).then((user) => {
-        if(!user) {
-            return res.status(404).send('No user found')
+    try {
+        // mongoose will automatically convert string id into object ID
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send('User not found')
         }
         res.send(user)
-        })
-        .catch((error) => res.status(500).send(error.message))
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
 // Post new task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-    task.save()
-        .then(() => res.status(201).send(task))
-        .catch((error) => res.status(400).send(error))
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
 })
 
 // Fetch all tasks
-app.get('/tasks', (req, res) => {
-    Task.find({})
-        .then((tasks) => res.send(tasks))
-        .catch((error) => res.status(500).send(error.message))
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
 // Fetch task by id
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
     if (!mongoose.isValidObjectId(_id)){
         return res.status(400).send('Invalid ID!')
     }
-    Task.findById(_id).then((task) => {
-        if(!task) {
-            res.status(404).send('No task found')
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send('No task found')
         }
         res.send(task)
-        .catch((error) => res.status(500).send(error.message))
-    })
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
 app.listen(port, () => {
