2e91d304cb9594cc4214dd49d543ed66f79d0477
Laura <lmcsay@gmail.com>
2021-01-23 09:22:07 -0600

    Feat: Generate web token upon login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,6 +43,16 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// generates token
+// will use this keyword so can't use arrow function
+// our methods are accessible on the instances sometimes instance methods
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign( {_id:user._id.toString() }, 'thisisthesecret');
+    return token;
+}
+
+// static methods are accessible on the model, sometimes called model methods
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne( { email })
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -22,8 +22,9 @@ router.post('/users', async (req, res)=> {
 router.post('/users/login', async (req, res) => {
     try {
         // creating our own function findByCredentials
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ user, token })
     } catch (error) {
         res.status(400).send()
     }
