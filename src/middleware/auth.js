c3ae03ad85c184e7955f43b36566c82d45f9960e
Laura <lmcsay@gmail.com>
2021-01-23 12:12:51 -0600

    Feat: Create token auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'thisisthesecret');
+
+        // find the user with the correct id who has that authentication token still stored
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {  
+            throw new Error();
+        }
+
+        // store information of user we found into req header 
+        // will provide user info to the route handler so it doesn't have to search for the user again
+        req.user = user
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'Please Authenticate' });
+    }
+};
+
+module.exports = auth;
