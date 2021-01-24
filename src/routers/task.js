330585ee6799a0d79d13e7758fbde755c8c186a2
Laura <lmcsay@gmail.com>
2021-01-23 20:20:29 -0600

    Refactor: Only update task made by current user

--- src/routers/task.js
+++ src/routers/task.js
@@ -53,8 +53,8 @@ router.get('/tasks/:id', auth, async (req, res) => {
 })
 
 // Update task by id
-router.patch('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
     if (!mongoose.isValidObjectId(_id)){
         return res.status(400).send('Invalid ID') 
     }
@@ -68,13 +68,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(_id);
-        updates.forEach(update => task[update] = req.body[update]);
-        await task.save();
-        
+        const task = await Task.findOne({ _id, owner: req.user._id })
+
         if (!task) {
             return res.status(404).send('No task found with given id')
         }
+        
+        updates.forEach(update => task[update] = req.body[update]);
+        await task.save();
         res.send(task)
     } catch (error) {
         res.status(500).send(error.message)
