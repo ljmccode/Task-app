efc68692edc795bad4b147bbac1ed675afbad76b
Laura <lmcsay@gmail.com>
2021-01-24 10:16:51 -0600

    Feat: Add pagination to tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -37,10 +37,11 @@ router.get('/tasks', auth, async (req, res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match
-            // match: {
-            //     completed: false
-            // }
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (error) {
