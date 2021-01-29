016306721bdd06c3e9a850a719da829cdfd2dad6
Laura <lmcsay@gmail.com>
2021-01-28 20:56:05 -0600

    Fix: Add catch to sgMail functions

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,22 +2,27 @@
 
  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendWelcomeEmail = (email, name) => {
+const sendWelcomeEmail = async (email, name) => {
     sgMail.send({
         to: email, 
         from: 'lmcsay@gmail.com',
         subject: 'Welcome to our website!',
         text: `Welcome, ${name}. We hope you enjoy our content.`
-    })
+    }).catch((e) => {
+        console.log(`email send error: ${e.message}`);
+    });
 }
 
-const sendCancellationEmail = (email, name) => {
+const sendCancellationEmail = async (email, name) => {
     sgMail.send({
         to: email, 
         from: 'lmcsay@gmail.com',
         subject: 'Account cancelled',
         text: `We are sorry to see you go, ${name}. Please take a moment to tell us why you cancelled your account.`
-    })
+    }).catch((e) => {
+        console.log(`email send error: ${e.message}`);
+
+    });
 }
 
 module.exports = {
