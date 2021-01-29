fd9c52482aa01d193faf34b0e9fd4ebe3a4a7221
Laura <lmcsay@gmail.com>
2021-01-29 12:13:35 -0600

    Style: Add additional testing ideas

--- tests/user.test.js
+++ tests/user.test.js
@@ -115,3 +115,11 @@ test('Should not update invalid user fields', async () => {
         .send({ location: 'France'})
         .expect(400)
 })
+
+//
+// Additional User Test Ideas
+//
+// Should not signup user with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
