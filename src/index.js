b52f77051307e693cc1eabfdda5d55e7463e3602
Laura <lmcsay@gmail.com>
2021-01-28 20:55:04 -0600

    Style: Move some contents of index.js to app.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-// Automatically parses JSON to an object
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
